723193f583074d823ea5cb6acad658c2f4f8d394
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-06-13 21:11:02 +0530

    Create Solution.java

---
+++ Day 4: Class vs. Instance/Solution.java
@@ -0,0 +1,30 @@
+
+
+public class Person {
+private int age;    
+  
+    public Person(int initialAge) {
+          if(initialAge < 0){
+            age = 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        }else{
+            age = initialAge;
+        }
+    }
+
+    public void amIOld() {
+        String ans;
+          if(age < 13){
+            ans = "You are young.";
+        }else if(age >= 13 && age < 18){
+            ans = "You are a teenager.";
+        }else{
+            ans = "You are old.";
+        }
+        System.out.println(ans);
+    }
+
+    public void yearPasses() {
+          age += 1;
+    }
+
